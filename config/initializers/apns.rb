d23b0e259912e24871d9ea7311de67abd760f4da
Touseef Murtaza <touseef.murtaza@nxb.com.pk>
2017-04-17 11:06:05 +0500

    APNS development setting added for production

--- config/initializers/apns.rb
+++ config/initializers/apns.rb
@@ -3,19 +3,13 @@ if Rails.env == "development"
   # gateway.sandbox.push.apple.com is default and only for development
   # gateway.push.apple.com is only for production
   APNS.port = 2195
-  # this is also the default. Shouldn't ever have to set this, but just in case Apple goes crazy, you can.
   APNS.pem  = '/home/touseef/Downloads/ChallanShor/APNS_Certificates_Dev.pem'
-  # this is the file you just created
   APNS.pass = ''
-  # Just in case your pem need a password
 else
-  APNS.host = 'gateway.push.apple.com'
-  # gateway.sandbox.push.apple.com is default and only for development
-  # gateway.push.apple.com is only for production
+  APNS.host = 'gateway.sandbox.push.apple.com'
+  # APNS.host = 'gateway.push.apple.com'
   APNS.port = 2195
-  # this is also the default. Shouldn't ever have to set this, but just in case Apple goes crazy, you can.
-  APNS.pem  = '/home/touseef/Downloads/ChallanShor/APNS_Certificates_Production.pem'
-  # this is the file you just created
+  # APNS.pem  = '/home/deploy/pem-files/APNS_Certificates_Production.pem'
+  APNS.pem  = '/home/deploy/pem-files/APNS_Certificates_Dev.pem'
   APNS.pass = ''
-  # Just in case your pem need a password
 end
