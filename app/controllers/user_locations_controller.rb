6744beeda8efb8f3eca26ff21c4e3b3f608691f2
Touseef Murtaza <touseef.murtaza@nxb.com.pk>
2017-04-17 15:08:27 +0500

    FCM notification json fixed

--- app/controllers/user_locations_controller.rb
+++ app/controllers/user_locations_controller.rb
@@ -1,10 +1,9 @@
 class UserLocationsController < ApplicationController
-
   def create
     @location = UserLocation.new(latitude: params[:latitude], longitude: params[:longitude], user_id: params[:user_id])
     if @location.save
       if @location.user.distance.nil?
-        distance = 20
+        distance = 20.0
       else
         distance = @location.user.distance
       end
@@ -27,7 +26,7 @@ class UserLocationsController < ApplicationController
         if @location.user.allow_notifications?
           if @location.user.device.device_type.eql?("android")
             fcm = FCM.new(ENV['FCM_API_KEY'])
-            options={:notification => {:body => {:data=>"#{alert}"},:title => "#{mark_type} alert"}}
+            options = {:data => {:body => "#{alert}", :title => "#{mark_type} alert"}}
             response = fcm.send(registration_ids, options)
           elsif @location.user.device.device_type.eql?("iOS")
             APNS.send_notification(device_token, :alert => "#{alert}", :badge => 1, :sound => 'default')
