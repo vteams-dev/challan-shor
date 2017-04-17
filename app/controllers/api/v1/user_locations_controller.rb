3e1db0280b38ff38f8ecd9238614a7809c506486
Touseef Murtaza <touseef.murtaza@nxb.com.pk>
2017-04-17 15:34:50 +0500

    Android push notification message fixed

--- app/controllers/api/v1/user_locations_controller.rb
+++ app/controllers/api/v1/user_locations_controller.rb
@@ -11,13 +11,16 @@ module Api
         @location = UserLocation.new(latitude: params[:latitude], longitude: params[:longitude], user_id: params[:user_id])
         if @location.save
           if @location.user.distance.nil?
-            distance = 20
+            distance = 20.0
           else
             distance = @location.user.distance
           end
           @markups = MarkDanger.near([@location.latitude,@location.longitude],distance)
-          unless @markups.empty?
-            marker = @markups.first
+          all_markers = @markups
+          user_markers = MarkDanger.where(:user_id => @location.user.id)
+          other_markers = all_markers - user_markers
+          unless other_markers.empty?
+            marker = other_markers.first
             distance = marker.distance.round(3)
             mark_type = MarkDanger::MARK_TYPE[marker.mark_type]
             device_token = @location.user.device.device_token
@@ -26,12 +29,12 @@ module Api
             if (distance < 0.009)
               alert = "You are on #{mark_type} place."
             else
-              alert = "There is #{mark_type} about #{distance} miles away."
+              alert = "You are #{distance} miles away from #{mark_type}."
             end
             if @location.user.allow_notifications?
               if @location.user.device.device_type.eql?("android")
                 fcm = FCM.new(ENV['FCM_API_KEY'])
-                options={:notification => {:body => {:data=>"#{alert}"},:title => "#{mark_type} alert"}}
+                options = {:data => {:body => "#{alert}", :title => "#{mark_type} alert"}}
                 response = fcm.send(registration_ids, options)
               elsif @location.user.device.device_type.eql?("iOS")
                 APNS.send_notification(device_token, :alert => "#{alert}", :badge => 1, :sound => 'default')
